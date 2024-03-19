9a74059b712a415708dda31fc87dcb9f26b110be
Daniel Schuba <daniel.schuba@footage.one>
2024-03-19 17:10:11 +0100

    chore: update NX to 18.1 and Angular 17.2

--- libs/rxjs/operators/vite.config.ts
+++ libs/rxjs/operators/vite.config.ts
@@ -6,6 +6,7 @@ import dts from 'vite-plugin-dts';
 import { join } from 'path';
 
 export default defineConfig({
+  root: __dirname,
   cacheDir: '../../../node_modules/.vite/rxjs-operators',
 
   plugins: [
@@ -30,6 +31,9 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
+    outDir: '../../../dist/libs/rxjs/operators',
+    reportCompressedSize: true,
+    commonjsOptions: { transformMixedEsModules: true },
     minify: false,
     lib: {
       // Could also be a dictionary or array of multiple entry points.
