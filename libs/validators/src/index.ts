4b2de19538bda6702ec03211c2fa787724c8b565
Daniel Schuba <daniel.schuba@footage.one>
2024-05-03 07:47:28 +0200

    chore: structure

--- libs/validators/src/index.ts
+++ libs/validators/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/hex-validator.directive';
+export * from './lib/hex/hex.validator';
+export * from './lib/hex/hex-validator.directive';
+export * from './lib/number/native-number-validator.directive';
+export * from './lib/number/number-validator.directive';
