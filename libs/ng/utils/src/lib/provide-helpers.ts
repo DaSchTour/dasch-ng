f0b8c77022262b5c713d3b899a5917160f427437
Daniel Schuba <daniel.schuba@footage.one>
2025-05-13 08:21:22 +0200

    chore: update to NX 21

---
+++ libs/ng/utils/src/lib/provide-helpers.ts
@@ -0,0 +1,22 @@
+import { FactoryProvider, InjectionToken, Type, ValueProvider } from '@angular/core';
+
+export function provideValue<T>(provide: InjectionToken<T> | Type<T>, useValue: T, multi?: boolean): ValueProvider {
+  return {
+    provide,
+    useValue,
+    multi,
+  };
+}
+
+export function provideFactory<T, U extends Array<any> = []>(
+  provide: InjectionToken<T> | Type<T>,
+  useFactory: (...args: U) => T,
+  { deps, multi }: { deps?: any[]; multi?: boolean } = {}
+): FactoryProvider {
+  return {
+    provide,
+    useFactory,
+    deps,
+    multi,
+  };
+}
