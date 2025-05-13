a1d65695b1159647e8014216810b7e55b6d33255
Daniel Schuba <daniel.schuba@footage.one>
2025-05-13 08:24:45 +0200

    build(gravatar): fix index export

--- libs/gravatar/src/index.ts
+++ libs/gravatar/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/generate-gravatar-link';
+export * from './lib/gravatar.directive';
 export * from './lib/gravatar.module';
 export * from './lib/gravatar.pipe';
 export * from './lib/gravatar.service';
